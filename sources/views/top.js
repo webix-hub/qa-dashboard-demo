aa7b0269925a2cf70bb124bafd689419e986df18
Nastya <akovalenko@xbsoftware.com>
2018-09-07 14:46:46 +0300

    [fix] toolbar alignment

--- sources/views/top.js
+++ sources/views/top.js
@@ -7,41 +7,51 @@ export default class TopView extends JetView {
 		return {
 			rows:[
 				{
-					view:"toolbar", height:56,
+					view:"toolbar",
+					height:56,
 					elements:[
 						{
-							view:"icon", icon:"menu",
-							click:() => this.app.callEvent("menu:toggle")
-						},
-						{
-							view:"label", template:"QA Dashboard", css:"main_label"
-						},
-						{},
-						{
-							view:"icon", icon:"bell",
-							badge:2,
-							tooltip:"Open latest notifications",
-							click: function(){
-								this.$scope.notifications.showLatest(this.$view);
-							}
-						},
-						{
-							view:"icon",
-							icon:"theme-light-dark",
-							tooltip:"Come to the dark side",
-							click:function(){
-								let theme = "light";
-								if (this.config.tooltip.indexOf("dark") !== -1) {
-									this.config.tooltip = "Back to the light side of the Force";
-									theme = "dark";
+							paddingY:4,
+							rows:[
+								{
+									cols:[
+										{
+											view:"icon", icon:"menu",
+											click:() => this.app.callEvent("menu:toggle")
+										},
+										{
+											view:"label", template:"QA Dashboard", css:"main_label"
+										},
+										{},
+										{
+											view:"icon", icon:"bell",
+											badge:2,
+											tooltip:"Open latest notifications",
+											click: function(){
+												this.$scope.notifications.showLatest(this.$view);
+											}
+										},
+										{
+											view:"icon",
+											icon:"theme-light-dark",
+											tooltip:"Come to the dark side",
+											click:function(){
+												let theme = "light";
+												if (this.config.tooltip.indexOf("dark") !== -1) {
+													this.config.tooltip = "Back to the light side of the Force";
+													theme = "dark";
+												}
+												else if (this.config.tooltip.indexOf("light") !== -1) {
+													this.config.tooltip = "Come to the dark side";
+													theme = "light";
+												}
+												this.refresh();
+												this.$scope.app.callEvent("change:theme",[theme]);
+											}
+										}
+									]
 								}
-								else if (this.config.tooltip.indexOf("light") !== -1) {
-									this.config.tooltip = "Come to the dark side";
-									theme = "light";
-								}
-								this.refresh();
-								this.$scope.app.callEvent("change:theme",[theme]);
-							}
+							]
 						}
 					]
 				},
