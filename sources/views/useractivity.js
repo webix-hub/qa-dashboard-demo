991c8e5d4c4c46646ff1063f9a3432651df9e211
Nastya <akovalenko@xbsoftware.com>
2018-10-09 12:36:06 +0300

    [update] paddings for geochart

--- sources/views/useractivity.js
+++ sources/views/useractivity.js
@@ -8,19 +8,24 @@ export default class UserActivityView extends JetView {
 			rows:[
 				{ type:"header", template:"User activity" },
 				{
-					view:"geochart",
-					localId:"geo",
-					// provide your own Google API key 
-					// https://developers.google.com/maps/documentation/javascript/get-api-key
-					key:"AIzaSyAi0oVNVO-e603aUY8SILdD4v9bVBkmiTg",
-					chart:{
-						colorAxis:{
-							colors:[ "#FDBF4C", "#1CA1C1", "#FF5C4C" ]
-						},
-						legend:"none",
-						datalessRegionColor:"#D9D8D7",
-						projection:"kavrayskiy-vii"
-					}
+					type:"form", rows:[
+						{
+							view:"geochart",
+							localId:"geo",
+							borderless:true,
+							// provide your own Google API key 
+							// https://developers.google.com/maps/documentation/javascript/get-api-key
+							key:"AIzaSyAi0oVNVO-e603aUY8SILdD4v9bVBkmiTg",
+							chart:{
+								colorAxis:{
+									colors:[ "#FDBF4C", "#1CA1C1", "#FF5C4C" ]
+								},
+								legend:"none",
+								datalessRegionColor:"#D9D8D7",
+								projection:"kavrayskiy-vii"
+							}
+						}
+					]
 				}
 			]
 		};
