6e4fc6f666a436a70b84b87a50bdb441b7f1b81e
Nastya <akovalenko@xbsoftware.com>
2018-10-09 13:07:34 +0300

    [fix] macon the undead

--- sources/models/qateam.js
+++ sources/models/qateam.js
@@ -12,7 +12,7 @@ const qateam = [
 	{"id":7,"name":"Oryx Leucoryx","stars":3,"tasks":326,"category":"Test Execution Engine","photo":"oryx"},
 	{"id":8,"name":"Zeb Kermode","stars":5,"tasks":438,"category":"Test Execution Engine","photo":"zeb"},
 	{"id":9,"name":"Morgan Yu","stars":5,"tasks":326,"category":"Test Execution Engine","photo":"morgan"},
-	{"id":10,"name":"Macon Dead, Jr.","stars":3,"tasks":125,"category":"Management","photo":"macon"},
+	{"id":10,"name":"Macon Alive, Jr.","stars":3,"tasks":125,"category":"Management","photo":"macon"},
 	{"id":11,"name":"Emily Kaldwin","stars":5,"tasks":658,"category":"Automation","photo":"emily"},
 	{"id":12,"name":"William Price","stars":4,"tasks":225,"category":"Product Quality Assurance","photo":"william"}
 ];
